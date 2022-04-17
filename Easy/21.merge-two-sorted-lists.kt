991bcc6c83a05e6ad4f25ef4a964ea78ab03ad7a
watarushi <watarutakamine@gmail.com>
2022-04-18 01:47:00 +0900

    21.merge-two-sorted-lists.kt

---
+++ Easy/21.merge-two-sorted-lists.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+import structure.ListNode
+
+class Solution {
+  fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+     list1?.let{
+
+     }
+  }
+}
